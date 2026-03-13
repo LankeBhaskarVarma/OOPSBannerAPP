5515d4dd5b086e92ec32d3e548fe174a69d25ae3
LankeBhaskarVarma <bl6597@srmist.edu.in>
2026-03-13 10:36:29 +0530

    added uc6

---
+++ OOPSBannerApp.java
@@ -0,0 +1,45 @@
+public class OOPSBannerApp {
+    // Static methods for each letter
+    static String[] getO() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    static String[] getP() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    static String[] getS() {
+        return new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+        // Array of letters in order
+        String[][] letters = {getO(), getO(), getP(), getS()};
+
+        // Loop through each row
+        for (int i = 0; i < 5; i++) {
+            for (String[] letter : letters) {
+                System.out.print(letter[i] + "  "); // Space between letters
+            }
+            System.out.println();
+        }
+    }
+}
